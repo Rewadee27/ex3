d7ed6d7e38c4c236b283fb69323e98d7dacc1ecd
Rewadee <59143438@g.cmru.ac.th>
2019-07-13 16:10:07 +0700

    ex3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var contact = require("../config/database")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'โคตรงง' });
+  var query = "select * from positions";
+  contact.query(query, function(error,result){
+    res.json(result)
+  })
+
+  
+ 
 });
 
 router.get('/about', function(req, res, next) {
